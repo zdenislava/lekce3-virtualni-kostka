b0092e0ae738dfaa131c47de602607059f94da21
zdenislava <navarikova@seznam.cz>
2021-03-23 20:09:08 +0100

    done

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // tady je místo pro náš program
+
+function diceRoll() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("zprava").innerHTML = "Hodila jsi " + roll;
+
+    document.querySelector("#kostka").src = "obrazky/" + roll + ".png";
+
+    if (roll == 6) {
+       zprava.innerHTML = "Hodila jsi 6, vyhrála jsi!";
+    }
+    else {
+        zprava.innerHTML = "Hodila jsi " + roll + ", házej znova";
+    };
+}
